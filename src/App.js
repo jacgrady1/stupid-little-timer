7ca2c28f6f38c36eb6c561b8833e8e81bc4447ce
jacgrady1 <lgxjackyzyx@gmail.com>
2017-08-09 22:51:44 -0700

    fix default sec

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     }
     clearState() {
         return {
-            currentSec: 30,
+            currentSec: 60,
             isStartDisabled: false
         };
     }
